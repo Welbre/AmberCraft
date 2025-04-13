8b7fb44bbd00c04ee5742c6523cd9dd9b184bc22
Welber <welbersrze@gmail.com>
2025-04-12 22:49:41 -0300

    init

---
+++ src/main/java/welbre/ambercraft/Main.java
@@ -0,0 +1,12 @@
+package welbre.ambercraft;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod("ambercraft")
+public class Main {
+    public Main(IEventBus modBus, ModContainer container) {
+        // Initialize logic here
+    }
+}
